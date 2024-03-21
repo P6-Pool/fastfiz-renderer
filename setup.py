d7a9703dfeb6a5bbe2108a2d8a9cbde3f8eaeadf
Mads Risager <madssr2@gmail.com>
2024-03-21 15:13:18 +0100

    Update reqs

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from grpc_tools import protoc
+import os
 
-with open('requirements.txt') as f:
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        compile_protobufs()
+
+
+def compile_protobufs():
+    proto_dir = "./fastfiz-renderer-protobuffers"
+    os.makedirs("fastfiz_renderer/compiled_protos", exist_ok=True)
+    protoc.main(
+        [
+            "--pyi_out=./fastfiz_renderer/compiled_protos",
+            "--python_out=./fastfiz_renderer/compiled_protos",
+            "--grpc_python_out=./fastfiz_renderer/compiled_protos",
+            "--proto_path=" + proto_dir,
+            proto_dir + "/api.proto",
+        ]
+    )
+
+
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='fastfiz_renderer',
-    version='0.0.1',
+    name="fastfiz_renderer",
+    version="0.0.1",
     packages=find_packages(),
-    license='MIT',
+    license="MIT",
     install_requires=requirements,
-    py_modules=['fastfiz_renderer']
+    py_modules=["fastfiz_renderer"],
+    cmdclass={"install": CustomInstall},
 )
-
-# python -m grpc_tools.protoc  --python_out=./fastfiz_renderer/compiled_protos --pyi_out=./fastfiz_renderer/compiled_protos  --grpc_python_out=./fastfiz_renderer/compiled_protos  --proto_path=./fastfiz-renderer-protobuffers ./fastfiz-renderer-protobuffers/api.proto
-# from . import api_pb2 as api__pb2
-
